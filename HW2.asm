793da2ea4239948381ee0d714ebcbd86908679b8
mkim797 <39310405+mkim797@users.noreply.github.com>
2020-04-15 16:45:44 -0700

    Update HW2.asm
    Version 2

--- HW2.asm
+++ HW2.asm
@@ -1,16 +1,27 @@
 	.data
-	newline:	.asciiz "\n"
-	xequal:		.asciiz "x = "
-	yequal:		.asciiz "y = "
+	xequal:		.asciiz "\n\tx = "
+	yequal:		.asciiz "\n\ty = "
+	calc:		.asciiz "\nCalculated values:"
 	enterx:		.asciiz "Enter initial x value: "
 	entery:		.asciiz "Enter initial y value: "
+	
 	.text
 	
-main:	li	$t1,	5
+	li	$t1,	5
 	li	$t2,	-3
 	li	$t3,	25
-	li	$s1,	25
-	li	$s2,	50
+main:	li	$v0,	4
+	la	$a0,	enterx
+	syscall
+	li	$v0,	5
+	syscall
+	move	$s1,	$v0
+	li	$v0,	4
+	la	$a0,	entery
+	syscall
+	li	$v0,	5
+	syscall
+	move	$s2,	$v0
 	
 x1:	bge	$s1,	$t1,	x2
 	addi	$s1,	$s1,	2
@@ -22,18 +33,18 @@ y1:	ble	$s2,	$t2,	y2
 	blt	$s2,	$t3,	y3
 	
 y2:	sub	$s2,	$s2,	$s1
-	j	exit
+	j	disp
 	
 y3:	add	$s2,	$s2,	$s1
 
-exit:	li	$v0,	4
+disp:	li	$v0,	4
+	la	$a0,	calc
+	syscall
+	li	$v0,	4
 	la	$a0,	xequal
 	syscall
 	li	$v0,	1
 	move	$a0,	$s1
-	syscall
-	li	$v0,	4
-	la	$a0,	newline
 	syscall
 	li	$v0,	4
 	la	$a0,	yequal
@@ -41,5 +52,6 @@ exit:	li	$v0,	4
 	li	$v0,	1
 	move	$a0,	$s2
 	syscall
-	li	$v0,	10
+
+exit:	li	$v0,	10
 	syscall
